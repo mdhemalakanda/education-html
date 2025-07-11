e4015cd16d228ed6b3c0baf4d80615211d37fce7
mdhemalakanda <hemal.akanda.39@gmail.com>
2025-07-11 23:29:40 +0600

    updated

--- index.js
+++ index.js
@@ -17,6 +17,36 @@ app.get('/', function(req, res) {
 app.get('/online-class', function(req, res) {
     res.render('index-2', { site_title: 'EduClass - An Online Education Template.'} );
 });
+app.get('/course-tab-open-filter', function(req, res) {
+    res.render('course-tab-open-filter', { site_title: 'Course - An Online Education Template.'} );
+});
+app.get('/course-tab-with-sort', function(req, res) {
+    res.render('course-tab-with-sort', { site_title: 'Course - An Online Education Template.'} );
+});
+app.get('/course-tab-with-collapsed-filter', function(req, res) {
+    res.render('course-tab-with-collapsed-filter', { site_title: 'Course - An Online Education Template.'} );
+});
+app.get('/course-left-sidebar', function(req, res) {
+    res.render('course-left-sidebar', { site_title: 'Course - An Online Education Template.'} );
+});
+app.get('/course-right-sidebar', function(req, res) {
+    res.render('course-right-sidebar', { site_title: 'Course - An Online Education Template.'} );
+});
+app.get('/course-left-collapsed-sidebar', function(req, res) {
+    res.render('course-left-collapsed-sidebar', { site_title: 'Course - An Online Education Template.'} );
+});
+app.get('/course-left-offcanvas-sidebar', function(req, res) {
+    res.render('course-left-offcanvas-sidebar', { site_title: 'Course - An Online Education Template.'} );
+});
+app.get('/course-right-offcanvas-sidebar', function(req, res) {
+    res.render('course-right-offcanvas-sidebar', { site_title: 'Course - An Online Education Template.'} );
+});
+app.get('/course-right-collapsed-sidebar', function(req, res) {
+    res.render('course-right-collapsed-sidebar', { site_title: 'Course - An Online Education Template.'} );
+});
+app.get('/course-details', function(req, res) {
+    res.render('course-details', { site_title: 'Course Details - An Online Education Template.'} );
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,54 @@
 (function($) {
     "use strict";
-   $(document).ready(function() {
+   $(document).ready(function(e) {
+        // Toogle text and show more btn.
+       $(document).on('click', '.ed-course-archive-show-more-btn', function(e) {
+            e.preventDefault();
+
+            $('.ed-course-description-content').toggleClass('ed-visible');
+
+            const $btn = $(this);
+            const $text = $btn.find('span');
+            const $icon = $btn.find('i');
+
+            $text.text($text.text().trim() === 'Show more' ? 'Show less' : 'Show more');
+            $icon.toggleClass('fa-angle-down fa-angle-up');
+        });
+
+
+
+
+        // custom accordion.
+        $(document).on( 'click', '.ed-course-material-accordion-head', function(e) {
+            const $this = $(e.currentTarget);
+
+            $this.closest('.ed-coruse-material-accordion-single').find('.ed-course-material-accordion-body-wrapper').slideToggle(500);
+            $this.closest('.ed-coruse-material-accordion-single').toggleClass('ed-collapsed');
+        } )
+
+        $(document).on( 'click', '.ed-filter-collapse', function(e) {
+            const $this = $(e.currentTarget);
+            $this.closest('.ed-filter-widget').find('.ed-filter-widget-list-wrapper').slideToggle(500);
+            $this.find('i').toggleClass('fa-minus').toggleClass('fa-plus');
+        } )
+
+        $(document).on('click', '.ed-toggle-course-filter-collapsed', function() {
+            $('.ed-course-archive-filter-collapsed').slideToggle(500);
+            $('.ed-archive-course-col-sidebar').toggleClass('ed-archive-course-sidebar-collapsed');
+            $('.ed-offcanvas-overlay').toggleClass('active');
+            $('.ed-course-archive-offcanvas').toggleClass('active');
+        })
+
+        $('.ed-offcanvas-close-btn').on( 'click', function() {
+            $('.ed-offcanvas-overlay').toggleClass('active');
+            $('.ed-course-archive-offcanvas').toggleClass('active');
+        } )
+
+        $('.ed-offcanvas-overlay').on( 'click', function() {
+             $('.ed-offcanvas-overlay').toggleClass('active');
+            $('.ed-course-archive-offcanvas').toggleClass('active');
+        } )
+
         const bannerCarousel = new Swiper('.ed-banner-course-carousel-active', {
             loop: true,
             clickable: true,
